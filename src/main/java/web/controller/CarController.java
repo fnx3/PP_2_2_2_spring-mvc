a37e94de62defcbf2af3ef4b5b71fe4bf99a721d
fnx3 <vladislav.timoshka@gmail.com>
2024-04-08 19:25:58 +0300

    resolve some mistakes

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarService;
-import web.service.CarServiceImp;
 
 
 @Controller
