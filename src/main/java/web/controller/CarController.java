5b658bb794726c4873828307e24a398dd60ece69
fnx3 <vladislav.timoshka@gmail.com>
2024-04-08 19:23:51 +0300

    resolve some mistakes

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,5 +1,6 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +14,12 @@ import web.service.CarServiceImp;
 @RequestMapping("/cars")
 public class CarController {
 
-    private final CarService carService = new CarServiceImp();
+    private CarService carService = new CarServiceImp();
+
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
 
     @GetMapping()
     public String getCars(@RequestParam(value = "count", required  = false) Integer count, Model model) {
@@ -24,4 +30,5 @@ public class CarController {
         }
         return "/cars";
     }
+
 }
